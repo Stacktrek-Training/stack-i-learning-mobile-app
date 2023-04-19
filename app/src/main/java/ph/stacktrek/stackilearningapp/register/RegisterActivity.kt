4d41e8a1e3299c0b4d27773ff8811ff988863302
Ricky Jun De Chino <rickyjundechino@gmail.com>
2023-04-19 21:42:17 +0800

    Initial Commit

---
+++ app/src/main/java/ph/stacktrek/stackilearningapp/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package ph.stacktrek.stackilearningapp.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ph.stacktrek.stackilearningapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
