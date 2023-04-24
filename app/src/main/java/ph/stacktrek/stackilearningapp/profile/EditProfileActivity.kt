da1c1e2f1d2b014b166c6ad0a342c9f5bc7d3de4
John Marlo Gilbolinga <gilbolingamarlo@gmail.com>
2023-04-24 15:25:18 +0800

    Added User Edit

--- app/src/main/java/ph/stacktrek/stackilearningapp/profile/EditProfileActivity.kt
+++ app/src/main/java/ph/stacktrek/stackilearningapp/profile/EditProfileActivity.kt
@@ -2,6 +2,7 @@ package ph.stacktrek.stackilearningapp.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ph.stacktrek.stackilearningapp.R
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
