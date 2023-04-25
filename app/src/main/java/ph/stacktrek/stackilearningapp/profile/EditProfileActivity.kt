e9b8cea1e8a7ad79f29378c75ac637bafcb42208
John Marlo Gilbolinga <gilbolingamarlo@gmail.com>
2023-04-25 15:15:47 +0800

    added functions on Setting button Back button

--- app/src/main/java/ph/stacktrek/stackilearningapp/profile/EditProfileActivity.kt
+++ app/src/main/java/ph/stacktrek/stackilearningapp/profile/EditProfileActivity.kt
@@ -1,12 +1,25 @@
 package ph.stacktrek.stackilearningapp.profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ph.stacktrek.stackilearningapp.R
+import ph.stacktrek.stackilearningapp.databinding.ActivityEditProfileBinding
+import ph.stacktrek.stackilearningapp.databinding.ActivityProfileBinding
 
 class EditProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backbutton2.setOnClickListener(){
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/ph/stacktrek/stackilearningapp/profile/ProfileActivity.kt
+++ app/src/main/java/ph/stacktrek/stackilearningapp/profile/ProfileActivity.kt
@@ -1,12 +1,26 @@
 package ph.stacktrek.stackilearningapp.profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ph.stacktrek.stackilearningapp.R
+import ph.stacktrek.stackilearningapp.databinding.ActivityProfileBinding
+import ph.stacktrek.stackilearningapp.databinding.ActivityRegisterBinding
+import ph.stacktrek.stackilearningapp.login.LoginActivity
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.profilesetting.setOnClickListener(){
+            val intent = Intent(this, EditProfileActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
